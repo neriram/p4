f556bb2d3fbd7dd3478c4bbe7f5de264899e21b9
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-08-17 12:11:59 -0700

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-<v-app class='main'>
-  <Home />
-</v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import Home from './components/Home';
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
+#nav {
+  padding: 30px;
 
-  components: {
-    Home,
-  },
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  data: () => ({
-    //
-  }),
-};
-</script>
-<style scoped>
-main {
-  color: '#ffeb3b'
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
